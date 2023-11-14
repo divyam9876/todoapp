bb39cb20148763756ee77c6542603ad2ac5b30e8
divyam9876 <148296653+divyam9876@users.noreply.github.com>
2023-11-14 02:03:06 -0500

    remove innerhtml login.js

--- login.js
+++ login.js
@@ -1,16 +1,27 @@
-let LoginForm=document.getElementById("loginForm")
-if(LoginForm) LoginForm.addEventListener('submit', login)
- 
-function login(e){
+//let rform = document.getElementById('registrationForm')
+//if (rform) rform.addEventListener('submit',SignUp)
+
+function Login(e){
     e.preventDefault()
+    
+    username= document.getElementById("username").value,
+    password= document.getElementById("password").value
 
     let user={
-        username: document.getElementById("username").value,
-        userpassword: document.getElementById("password").value
-    }
-    let h3=document.getElementById("greeting")
-    h3.innerHTML = `Welcome Back ${user.username}!!`
-
-    window.location.href = "todo.html"
+        
+        username: username,
+        password:password
+      
+    };
+    console.log(user);
 }
-//console.log("Hello")
+
+document.addEventListener('DOMContentLoaded',function(){
+    let loginForm=document.getElementById('loginForm')
+    loginForm.addEventListener('submit',Login)
+})
+    
+    //registrationForm.innerHTML = `Welcome ${user.firstName} ${user.lastName}`
+
+
+    //window.location.href = "login.html";
