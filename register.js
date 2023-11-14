383f0078056f1581a7fe410fb326c51c980a2232
divyam9876 <148296653+divyam9876@users.noreply.github.com>
2023-11-14 02:03:41 -0500

    remove innerhtml in register

--- register.js
+++ register.js
@@ -1,17 +1,29 @@
-let rform = document.getElementById('registrationForm')
-if (rform) rform.addEventListener('submit',SignUp)
+//let rform = document.getElementById('registrationForm')
+//if (rform) rform.addEventListener('submit',SignUp)
 
 function SignUp(e){
     e.preventDefault()
+    firstname= document.getElementById("fName").value,
+    lastname= document.getElementById("lName").value,
+    username= document.getElementById("userName").value,
+    password= document.getElementById("userPassword").value
 
     let user={
-        firstName:document.getElementById("fName").value,
-        lastName:document.getElementById("lName").value,
-        username:document.getElementById("userName").value,
-        password:document.getElementById("userPassword").value
-    }
-    let h4=document.getElementById("userdetails")
-    h4.innerHTML = `Welcome ${user.firstName} ${user.lastName}`
-
-    window.location.href = "login.html";
+        firstName:firstname,
+        lastName: lastname,
+        username: username,
+        password:password
+      
+    };
+    console.log(user);
 }
+
+document.addEventListener('DOMContentLoaded',function(){
+    let registrationForm=document.getElementById('registrationForm')
+    registrationForm.addEventListener('submit',SignUp)
+})
+    
+    //registrationForm.innerHTML = `Welcome ${user.firstName} ${user.lastName}`
+
+
+    //window.location.href = "login.html";
